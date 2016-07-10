945ae88785ccd7b7d48139509a659ce41cb63d53
Matko Bulic <bulicmatko@gmail.com>
2016-07-10 20:04:46 +0200

    fix(menu): remove item from menu
    remove item from menu

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,6 @@ import RootContainer from '../containers/RootContainer';
 import HomePage from '../components/pages/HomePage';
 import Page1 from '../components/pages/Page1';
 import Page2 from '../components/pages/Page2';
-import Page3 from '../components/pages/Page3';
 
 /**
  *  Routes
@@ -31,10 +30,6 @@ export default [
       {
         path: '/app/page-2',
         component: Page2,
-      },
-      {
-        path: '/app/page-3',
-        component: Page3,
       },
     ],
   },
