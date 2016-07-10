d7f6ec58d8b3caa146c73d8e4e570d8266e9050d
Matko Bulic <bulicmatko@gmail.com>
2016-07-10 19:18:33 +0200

    feat(page): add new page
    add new page to demo

---
+++ src/components/pages/Page3/index.js
@@ -0,0 +1,19 @@
+/* eslint-env browser */
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  Page 3 - Index
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+import { connect } from 'react-redux';
+
+import Component from './Component';
+
+/**
+ *  Connector
+ */
+export default connect(
+  state => state,
+  dispatch => ({ dispatch })
+)(Component);
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ import RootContainer from '../containers/RootContainer';
 import HomePage from '../components/pages/HomePage';
 import Page1 from '../components/pages/Page1';
 import Page2 from '../components/pages/Page2';
+import Page3 from '../components/pages/Page3';
 
 /**
  *  Routes
@@ -30,6 +31,10 @@ export default [
       {
         path: '/app/page-2',
         component: Page2,
+      },
+      {
+        path: '/app/page-3',
+        component: Page3,
       },
     ],
   },
